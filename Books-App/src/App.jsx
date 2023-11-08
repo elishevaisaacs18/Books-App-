60c90c0dc2ba6cbd3a5a760107d68993d7bd9c53
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-08 11:43:02 +0200

    section 2 book exer

--- Books-App/src/App.jsx
+++ Books-App/src/App.jsx
@@ -1,11 +1,43 @@
 import "./App.css";
 import Book from "./Components/Book";
 function App() {
+  const book1 = {
+    title: "harry Potter",
+    author: "j.k. rolling",
+    onShelf: true,
+    likes: 21,
+    num: 1,
+    text: "hi1",
+  };
+
+  const book2 = {
+    title: "harry Potter 2",
+    author: "j.k. rolling",
+    onShelf: false,
+    likes: 200,
+    num: 2,
+    text: "hi2",
+  };
+
+  const book3 = {
+    title: "harry Potter 3",
+    author: "j.k. rolling",
+    onShelf: true,
+    likes: 1,
+    num: 3,
+    text: "hi3",
+  };
+
+  function takeBook(book) {
+    book.onShelf = false;
+    
+  }
+
   return (
     <div id="shelf">
-      <Book title="harry Potter" author="j.k. rolling" onShelf={true} likes={21} num={1}/>
-      <Book title="harry Potter 2" author="j.k. rolling" onShelf={false} likes={200} num={2}/>
-      <Book title="harry Potter 3" author="j.k. rolling" onShelf={true} likes={1} num={3}/>
+      <Book book={book1} onClick={() => takeBook(book1)} />
+      <Book book={book2} onClick={() => takeBook(book2)} />
+      <Book book={book3} onClick={() => takeBook(book3)} />
     </div>
   );
 }
--- Books-App/src/Components/Book.jsx
+++ Books-App/src/Components/Book.jsx
@@ -1,14 +1,51 @@
-import styles from "../Book.module.css"
+import styles from "../Book.module.css";
 const Book = (props) => {
+  console.log(props.book.author);
   return (
-    <div className={styles.book} style={{backgroundColor: props.onShelf ? "navajowhite": "white"}}>
-        {props.likes > 100 ? <h2>{props.title}</h2>: <p>{props.title}</p> }
-        <p>{props.author}</p>
-        <p>{props.onShelf}</p>
-        <p>{props.likes}</p>
-        <p>{props.num}</p>
-    </div>
-  )
-}
+    <div
+      className={styles.book}
+      style={{ backgroundColor: props.book.onShelf ? "navajowhite" : "white" }}
+    >
+      {props.book.likes > 100 ? (
+        <h2>{props.book.title}</h2>
+      ) : (
+        <p>{props.book.title}</p>
+      )}
 
-export default Book
+      <p>{props.book.author}</p>
+
+      {props.book.onShelf === true ? (
+        <>
+          <p>
+            {props.book.onShelf}
+            <button
+              onClick={(e) => {
+                props.onClick;
+                e.target.parentNode.parentNode.style.backgroundColor = "white";
+                e.target.parentNode.remove();
+              }}
+            >
+              Take Book
+            </button>
+          </p>
+        </>
+      ) : (
+        <p>{props.book.onShelf}</p>
+      )}
+      <>
+        <p id="likes">{props.book.likes}</p>
+        <p>{props.book.num}</p>
+        <button onClick={() => alert(props.book.text)}>click me</button>
+        <button
+          onClick={(e) => {
+            e.target.parentNode.children[3].textContent = ++props.book.likes;
+          }}
+        >
+          Like
+        </button>
+      </>
+    </div>
+  );
+};
+
+export default Book;
