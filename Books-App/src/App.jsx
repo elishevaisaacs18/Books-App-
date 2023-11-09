a51144a343fe5fb086f672335ce5af342b1f5b12
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-11-09 16:03:07 +0200

    section 4

--- Books-App/src/App.jsx
+++ Books-App/src/App.jsx
@@ -1,43 +1,40 @@
 import "./App.css";
 import Book from "./Components/Book";
 function App() {
-  const book1 = {
+  const books = [{
     title: "harry Potter",
     author: "j.k. rolling",
     onShelf: true,
     likes: 21,
     num: 1,
     text: "hi1",
-  };
-
-  const book2 = {
+  }, {
     title: "harry Potter 2",
     author: "j.k. rolling",
     onShelf: false,
     likes: 200,
     num: 2,
     text: "hi2",
-  };
-
-  const book3 = {
+  }, {
     title: "harry Potter 3",
     author: "j.k. rolling",
     onShelf: true,
     likes: 1,
     num: 3,
     text: "hi3",
-  };
+  }]
 
   function takeBook(book) {
     book.onShelf = false;
     
   }
 
+  const listItems = books.map((bookE) =>
+  <Book book={bookE} onClick={() => takeBook(bookE)} />);
+
   return (
     <div id="shelf">
-      <Book book={book1} onClick={() => takeBook(book1)} />
-      <Book book={book2} onClick={() => takeBook(book2)} />
-      <Book book={book3} onClick={() => takeBook(book3)} />
+      {listItems}
     </div>
   );
 }
--- Books-App/src/Components/Book.jsx
+++ Books-App/src/Components/Book.jsx
@@ -1,49 +1,45 @@
 import styles from "../Book.module.css";
 const Book = (props) => {
-  console.log(props.book.author);
+  const bookTitle =
+    props.book.likes > 100 ? (
+      <h2>{props.book.title}</h2>
+    ) : (
+      <p>{props.book.title}</p>
+    );
+  function bookOnShelf() {
+    return props.book.onShelf === true ? (
+      <p>
+        {props.book.onShelf}
+        <button
+          onClick={(e) => {
+            props.onClick;
+            e.target.parentNode.parentNode.style.backgroundColor = "white";
+            e.target.parentNode.remove();
+          }}
+        >
+          Take Book
+        </button>
+      </p>
+    ) : (
+      <p>{props.book.onShelf}</p>
+    );
+  }
+  function addLike(e) {
+    e.target.parentNode.children[3].textContent = ++props.book.likes;
+  }
+
   return (
     <div
       className={styles.book}
-      style={{ backgroundColor: props.book.onShelf ? "navajowhite" : "white" }}
+      style={{ backgroundColor: props.book.onShelf && "navajowhite" }}
     >
-      {props.book.likes > 100 ? (
-        <h2>{props.book.title}</h2>
-      ) : (
-        <p>{props.book.title}</p>
-      )}
-
+      {bookTitle}
       <p>{props.book.author}</p>
-
-      {props.book.onShelf === true ? (
-        <>
-          <p>
-            {props.book.onShelf}
-            <button
-              onClick={(e) => {
-                props.onClick;
-                e.target.parentNode.parentNode.style.backgroundColor = "white";
-                e.target.parentNode.remove();
-              }}
-            >
-              Take Book
-            </button>
-          </p>
-        </>
-      ) : (
-        <p>{props.book.onShelf}</p>
-      )}
-      <>
-        <p id="likes">{props.book.likes}</p>
-        <p>{props.book.num}</p>
-        <button onClick={() => alert(props.book.text)}>click me</button>
-        <button
-          onClick={(e) => {
-            e.target.parentNode.children[3].textContent = ++props.book.likes;
-          }}
-        >
-          Like
-        </button>
-      </>
+      {bookOnShelf()}
+      <p id="likes">{props.book.likes}</p>
+      <p>{props.book.num}</p>
+      <button onClick={() => alert(props.book.text)}>click me</button>
+      <button onClick={(e) => addLike(e)}>Like</button>
     </div>
   );
 };
